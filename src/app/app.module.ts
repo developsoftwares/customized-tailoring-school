07273e99c00664765dac164c7bb5d2f676ffd839
John Micheal <diamonds1759@gmail.com>
2019-06-20 16:42:40 +0100

    chore: Setting up bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  ],
+  exports: [
+    NavbarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
